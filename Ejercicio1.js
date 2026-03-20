46779f4d4bbafa1c80f98d584493f42d6322116a
juandavidcastrorubiano-svg <juandacastro1985@gmail.com>
2026-03-20 13:50:34 -0500

    Este es el final del ejercicio 1

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -24,10 +24,10 @@ let empleado1 = new Empleado( "Juan ", "Desarrollador ", 3000000)
 let empleado2 = new Empleado( "jose ", "Tecnico ", 2000000)
 let empleado3 = new Empleado( "Marcela ", "Gerente ", 30000000)
 
-//console.log(empleado1.presentarse())
-//console.log(empleado1.calcularSalarioAnual())
-//console.log(empleado1.desactivar())
-//console.log(empleado1.info())
+console.log(empleado1.presentarse())
+console.log(empleado1.calcularSalarioAnual())
+console.log(empleado1.desactivar())
+console.log(empleado1.info())
 
 
 
@@ -62,3 +62,33 @@ console.log(lider2.agregarMiembro("Felix"))
 console.log(lider2.presentarEquipo())
 console.log(lider1.info())
 console.log(lider2.info())
+
+let empleado4 = new Empleado("Max", "Auxiliar", 2000000)
+let empleado5 = new Empleado("Angie", "Auxiliar", 2000000)
+let empleado6 = new Empleado("Helena", "Auxiliar", 2000000)
+let empleado7 = new Empleado("Sofia", "Auxiliar", 2000000)
+
+let lider3 = new Lider("Laura", 6000000, ["Max", "Angie"])
+let lider4 = new Lider("Diego", 6000000, ["Helena", "Sofia"])
+
+let empleados = [ empleado4, empleado5, empleado6, empleado7, lider3, lider4]
+
+let total = 0
+
+for (let i = 0; i < empleados.length; i++) {
+    total += empleados[i].salario
+}
+// la nomina la realice onvace a todos los empleados desde el inicio del ejercicio
+console.log("Nomina mensual:", total)
+console.log("Nomina anual:", total * 12)
+
+// desactivar uno
+empleado6.desactivar()
+console.log(empleado6.info())
+
+// info lideres
+console.log(lider3.info())
+console.log(lider3.presentarEquipo())
+
+console.log(lider4.info())
+console.log(lider4.presentarEquipo())
